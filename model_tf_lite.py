908307535aeda0df7b7c4abb29f429a3967cdc67
hungphamNLP <82246573+hungphamNLP@users.noreply.github.com>
2023-03-28 21:36:36 +0700

    Update model_tf_lite.py

--- model_tf_lite.py
+++ model_tf_lite.py
@@ -1,5 +1,34 @@
 import tensorflow as tf
 import pickle
+import numpy as np
+import ffmpeg
+
+
+def load_audio(file: str, sr: int = 16000):
+    """
+    Open an audio file and read as mono waveform, resampling as necessary
+    Parameters
+    ----------
+    file: str
+        The audio file to open
+    sr: int
+        The sample rate to resample the audio if necessary
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
+    try:
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        out, _ = (
+            ffmpeg.input(file, threads=0)
+            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+        )
+    except ffmpeg.Error as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
 
 
 def _load_pickle(path):
@@ -25,13 +54,13 @@ class Model_Whisper_infer:
 
 
 
+
+
 if __name__ == '__main__':
-    from scipy.io import wavfile
-    import torchaudio
     filename ='./samples/jfk.wav'
-    data,rt = torchaudio.load(filename)
-    
-    #load model
+    data_ipt=load_audio(filename)
+    #print(data_ipt)
+
     tflite_model_path = './checkpoint/whisper.tflite'
 
     #load feature
@@ -42,5 +71,5 @@ if __name__ == '__main__':
 
     model = Model_Whisper_infer(tflite_model_path,feature_extractor,tokenizer_)
     
-    opt=model.infer(data[0])
+    opt=model.infer(data_ipt)
     print(opt)
